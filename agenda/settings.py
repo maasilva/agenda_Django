089d42154ed7da9db09f2c0570a1c0781f8d8fd4
Marcos A. Silva <marcossilva26@gmail.com>
2020-08-05 17:40:38 -0300

    Fix.v2 heroku config

--- agenda/settings.py
+++ agenda/settings.py
@@ -123,4 +123,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
